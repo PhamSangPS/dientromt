4fd9415d9f49e91ecdacc0591b5942b6b81e64d6
PhamSangPS <160474426+PhamSangPS@users.noreply.github.com>
2025-02-04 23:43:46 +0700

    Update Axios.js

--- client/src/utils/Axios.js
+++ client/src/utils/Axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import SummaryApi , { baseURL } from "../common/SummaryApi";
 
 const Axios = axios.create({
-    
-    Credentials : true
+    baseURL : baseURL,
+    withCredentials : true
 })
 
 //sending access token in the header
