fc6eab8269fd8456f39c21b5f01b42de774cc179
PhamSangPS <phamsangwork@gmail.com>
2025-02-04 16:29:39 +0700

    first commit

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaPhoneAlt } from "react-icons/fa";
+import { SiZalo } from "react-icons/si";
+
+
+const Footer = () => {
+  return (
+    <footer className='border-t'>
+        <div className='container mx-auto p-4 text-center flex flex-col lg:flex-row lg:justify-between gap-2'>
+            <p>© All Rights Reserved 2024.</p>
+
+            <div className='flex items-center gap-4 justify-center text-2xl'>
+                <a href='tel:19006413' className='hover:text-primary-100'>
+                    <FaPhoneAlt/>
+                </a>
+                <a href='https://zalo.me/0352627623' className='hover:text-primary-400'>
+                    <SiZalo />
+                </a>
+                <a href='' className='hover:text-primary-100'>
+                    <FaLinkedin/>
+                </a>
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
