d71e7df563bf377ed2eaea6a64ffe1c3032f731d
PhamSangPS <160474426+PhamSangPS@users.noreply.github.com>
2025-02-05 11:58:26 +0700

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -116,7 +116,7 @@ const Header = () => {
                                                     
                                                 </div>
                                             ) : (
-                                                <button onClick={redirectToLoginPage} className='text-lg px-2'></button>
+                                                <button onClick={redirectToLoginPage} className='text-lg px-2'>         </button>
                                             )
                                         }
                                         
